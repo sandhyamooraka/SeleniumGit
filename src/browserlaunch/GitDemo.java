d2812eb786734a57d95c8683f0d52c39bb7c8a7f
sandhyamooraka <103105943+sandhyamooraka@users.noreply.github.com>
2024-12-04 06:19:54 +0530

    Updtated code

--- src/browserlaunch/GitDemo.java
+++ src/browserlaunch/GitDemo.java
@@ -1,14 +1,20 @@
 package browserlaunch;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class GitDemo {
+	WebDriver driver ;
+	
   @Test
-  public void f() {
+  public void testGit() {
+	  driver.get("https://www.google.com");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
   }
 
 }
